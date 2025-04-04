a28568204ef950195f63b84b8043cfd127bf5f8c
Connor <goodca2021@mountunion.edu>
2025-04-03 21:18:45 -0400

    Updated

--- 2D_ISQ.py
+++ 2D_ISQ.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
-def find_energy(n):
-    return (n ** 2) * (np.pi ** 2) / 2
+def find_energy(nx, ny):
+    return (nx ** 2 + ny ** 2) * (np.pi ** 2) / 2
 
-def V(x):
-    return np.where((0 <= x) & (x <= 1), 0, 1e10)
+def V(x, y):
+    return np.where((0 <= x) & (x <= 1) & (0 <= y) & (y <= 1), 0, 1e10)
 
 def schrodinger(r, x, V, E):
     hbar = 1
@@ -21,7 +21,7 @@ def schrodinger(r, x, V, E):
         dphi = 0
     return [dpsi, dphi]
 
-def solver(n):
+def solver(nx, ny):
     resolution = 50
     min = -2
     max = 2
@@ -32,9 +32,10 @@ def solver(n):
     valid_y = [y for y in y_vals if 0 <= y <= 1]
 
     X, Y = np.meshgrid(valid_x, valid_y)
-    initial = [[0, 1]
 
-    E = find_energy(n)
+    initial = [[0, 1], [0, 1]]
+
+    E = find_energy(nx, ny)
 
     solution = odeint(schrodinger, initial, valid_x, args=(V, E))
     psi_vals = solution[:, 0]
@@ -50,47 +51,31 @@ def solver(n):
 
     return x_vals, psi_vals, E
 
-def plot_schrodinger(n):
-    fig, ax = plt.subplots(len(n), 2)
+def plot_schrodinger(states):
+    fig, ax = plt.subplots(len(states), 2)
 
-    for i, n in enumerate(n):
+    for i, (nx, ny) in enumerate(states):
 
-        x_vals, psi_vals, E = solver(n)
+        x_vals, psi_vals, E = solver(nx, ny)
 
-        left_bound = x_vals <= 0
-        middle = (x_vals > 0) & (x_vals < 1)
-        right_bound = x_vals >= 1
 
-        ax[i, 0].plot(x_vals[left_bound], psi_vals[left_bound], color='r')
-        ax[i, 0].plot(x_vals[middle], psi_vals[middle], color='g')
-        ax[i, 0].plot(x_vals[right_bound], psi_vals[right_bound], color='r')
 
-        ax[i, 0].set_ylim(-2, 2)
-
-        ax[i, 0].set_title("Wavefunction of n = {0}".format(n))
+        ax[i, 0].set_title("Wavefunction of n = {0}".format(nx, ny))
         ax[i, 0].axvline(0, color='k', linestyle='--')
         ax[i, 0].axvline(1, color='k', linestyle='--')
-        ax[i, 0].fill_between(x_vals[left_bound], 2, alpha=0.3, color='gray')
-        ax[i, 0].fill_between(x_vals[left_bound], -2, alpha=0.3, color='gray')
-        ax[i, 0].fill_between(x_vals[right_bound], 2, alpha=0.3, color='gray')
-        ax[i, 0].fill_between(x_vals[right_bound], -2, alpha=0.3, color='gray')
+
         ax[i, 0].set_xlabel("Position")
         ax[i, 0].set_ylabel("Psi")
         ax[i, 0].grid(True)
 
-        ax[i, 1].plot(x_vals[left_bound], psi_vals[left_bound] ** 2, color='r')
-        ax[i, 1].plot(x_vals[middle], psi_vals[middle] ** 2, color='g')
-        ax[i, 1].plot(x_vals[right_bound], psi_vals[right_bound] ** 2, color='r')
+
 
         ax[i, 1].set_ylim(-0.5, 2.5)
 
-        ax[i, 1].set_title("Probability of n = {0}".format(n))
+        ax[i, 1].set_title("Probability of n = {0}".format(nx, ny))
         ax[i, 1].axvline(0, color='k', linestyle='--')
         ax[i, 1].axvline(1, color='k', linestyle='--')
-        ax[i, 1].fill_between(x_vals[left_bound], 2.5, alpha=0.3, color='gray')
-        ax[i, 1].fill_between(x_vals[left_bound], -0.5, alpha=0.3, color='gray')
-        ax[i, 1].fill_between(x_vals[right_bound], 2.5, alpha=0.3, color='gray')
-        ax[i, 1].fill_between(x_vals[right_bound], -0.5, alpha=0.3, color='gray')
+
         ax[i, 1].set_xlabel("Position")
         ax[i, 1].set_ylabel("Prob. Density")
         ax[i, 1].grid(True)
@@ -98,4 +83,4 @@ def plot_schrodinger(n):
     plt.tight_layout()
     plt.show()
 
-plot_schrodinger([1, 2, 3, 4, 5])
+plot_schrodinger([(1, 1), (2, 1), (1, 2)])
